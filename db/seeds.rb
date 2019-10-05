19d5cbae7903ebe09cc1bb5507dc6fd11d5792d3
Hamed Waiz <hamedw96@mgmail.com>
2019-10-04 19:07:00 -0700

    Fix PokeLinks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1378,7 +1378,8 @@ ActiveRecord::Base.transaction do
   }
 
   pokemon = pokemon.map do |num, stats|
-    stats['image_url'] = "#{num}.svg"
+    # stats['image_url'] = "#{num}.svg"
+    stats['image_url'] = image_url("./pokemon_snaps/#{num}.svg")
     stats
   end
 
